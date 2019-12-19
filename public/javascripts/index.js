db3df281bbc5be6596b375614f304bcaa263df2d
darco2018 <newsecond@outlook.com>
2019-12-19 18:18:42 +0100

    Fix functionality. Improve style

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,9 @@
 window.addEventListener('DOMContentLoaded', event => {
   console.log('DOM fully loaded and parsed');
 
-  const movielist = document.getElementById('movieList');
-  const movieInput = document.getElementById('movieInput');
+  const futureList = document.getElementById('futureList');
+  const historyList = document.getElementById('historyList');
+  const titleInput = document.getElementById('titleInput');
 
   const baseUrl = 'http://localhost:3000';
   const apiUrl = '/api/movies/';
@@ -12,8 +13,10 @@ window.addEventListener('DOMContentLoaded', event => {
   init();
 
   async function init() {
+    console.log('init start');
     try {
       const movies = await getMovies(fullUrl);
+      console.log('init end');
       showMovies(movies);
     } catch (error) {
       console.error(
@@ -24,15 +27,21 @@ window.addEventListener('DOMContentLoaded', event => {
 
   /* --------------- event listeners ---------------------------- */
 
-  movieInput.addEventListener('keydown', async function fn(e) {
+  titleInput.addEventListener('keydown', async function fn(e) {
+    
     var key = e.which || e.keyCode || 0;
     if (key == 13) {
       try {
-        const movieTitle = movieInput.value;
+        const movieTitle = titleInput.value;
         // add year & genre
         const newMovie = { title: movieTitle };
-        movieInput.value = '';
+        titleInput.value = '';
+        debugger;
+        console.log("postMovie is about to be called in listener");        
         const savedMovie = await postMovie(fullUrl, newMovie);
+        console.log("savedMovie is :");
+        console.log(savedMovie);
+        console.log("postMovie finished in listener");    
         init();
       } catch (error) {
         'There has been a problem with your fetch operation: ' + error.message;
@@ -40,26 +49,40 @@ window.addEventListener('DOMContentLoaded', event => {
     }
   });
 
-  movielist.addEventListener('click', function fn(e) {
+  futureList.addEventListener('click', handleListClick);
+  historyList.addEventListener('click', handleListClick);
+
+  function handleListClick(e) {
+    
+    let clickedList = null;
+    if (this === futureList) {
+      clickedList = futureList;
+    } else {
+      clickedList = historyList;
+    }
+
     let listItem = getClosest(e.target, 'LI');
     const movieId = listItem.dataset.id;
 
     try {
       if (e.target.className === 'deleteBtn') {
-        movielist.removeChild(listItem);
+        //clickedList.removeChild(listItem);
         deleteMovie(movieId);
       } else {
-        const isWatched = toggleIsWatched(e);
-        updateMovie(movieId, { isWatched });
+        let isWatched = (clickedList === futureList) ? true : false;
+        updateMovie(movieId, { isWatched });        
       }
+      init();
     } catch (error) {
       'There has been a problem with your fetch operation: ' + error.message;
     }
-  });
+  }
 
   /* ------------------------- API calls ---------------------- */
 
   async function getMovies(url = '') {
+    console.log("GET movies start");
+    
     const res = await fetch(url, {
       method: 'GET', // default
       mode: 'cors', // default
@@ -72,10 +95,12 @@ window.addEventListener('DOMContentLoaded', event => {
     }
 
     const json = await res.json(); // parses JSON response into native JavaScript objects
+    console.log("GET movies end");
     return json.movies;
   }
 
   async function postMovie(url = '', newMovie = {}) {
+    console.log("POST movies start");
     const res = await fetch(url, {
       method: 'POST',
       mode: 'cors',
@@ -89,6 +114,7 @@ window.addEventListener('DOMContentLoaded', event => {
       return handleErrors(res);
     }
     const movie = await res.json();
+    console.log("POST movies end");
     return movie; // parses JSON response into native JavaScript objects
   }
 
@@ -120,16 +146,46 @@ window.addEventListener('DOMContentLoaded', event => {
     return movie;
   }
 
-  /* ----------------- helpers ----------------------------*/
+  /* ----------------- view rendering ----------------------------*/
 
   function showMovies(movies) {
-    movieList.innerHTML = '';
+    console.log("showMovies start");
+
+    futureList.innerHTML = '';
+    historyList.innerHTML = '';
+
+    for (const movie of movies) {
+      const movieElem = createMovieItem(movie);
+      if (movie.isWatched) {
+        historyList.appendChild(movieElem);
+      } else {
+        futureList.appendChild(movieElem);
+      }
+    }
+    console.log("showMovies end");
+  }
+
+  function createMovieItem(movie) {
+    const { _id, title, released, genre, rating, isWatched } = movie;
+    let listItem = document.createElement('LI');
+    listItem.setAttribute('data-id', _id);
+    listItem.innerHTML =
+      `<p class="movieItem">
+        <strong>${title}</strong> 
+       </p>`;
+  /*  released in <span>${released}</span>,  <span>${genre}</span> */
+
+    listItem.innerHTML += `<span class="deleteBtn">X</span>`;
+    return listItem;
+  }
+
+  function addToHistory() {
+    historyList.innerHTML = '';
 
     for (const movie of movies) {
       const { _id, title, released, genre, rating, isWatched } = movie;
       let listItem = document.createElement('LI');
-/*       listItem.classList.add('movieItem');
- */      listItem.setAttribute('data-id', _id);
+      listItem.setAttribute('data-id', _id);
       listItem.innerHTML =
         `<p class="${isWatched ? 'movieItem isWatched' : 'movieItem'}">` +
         `<strong>${title}</strong> 
@@ -139,25 +195,10 @@ window.addEventListener('DOMContentLoaded', event => {
 
       listItem.innerHTML += `<span class="deleteBtn">X</span>`;
 
-      movieList.appendChild(listItem);
+      futureList.appendChild(listItem);
     }
   }
-
-  function toggleIsWatched(e) {
-    let p =
-      e.target.tagName === 'LI'
-        ? e.target.children[0]
-        : getClosest(e.target, 'P');
-
-    let isWatched = false;
-    if (p.classList.contains('isWatched')) {
-      p.classList.remove('isWatched');
-    } else {
-      p.classList.add('isWatched');
-      isWatched = true;
-    }
-    return isWatched;
-  }
+  /* ----------------- helpers ----------------------------*/
 
   function getClosest(elem, selector) {
     for (; elem && elem !== document; elem = elem.parentNode) {
